d60eaa6362429b4ca5bb4ff2a8ae9d7f6aa3b259
bekpapio <bekatolesa2015@gmail.com>
2023-08-13 11:07:20 -0700

    footer update

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -9,38 +9,38 @@ const imgs = [
 export default function Footer() {
   var d = new Date();
   return (
-    <footer className="bg-black h-[36vh] text-white ">
-      <div className="flex mx-[5rem] justify-between pt-[1rem]">
+    <footer className="bg-black text-white w-full">
+      <div className="flex flex-col gap-5 sm:flex-row flex-wrap mx-[1rem] md:mx-[5rem] items-center justify-between pt-[1rem]">
         <p className="text-[30px] flex items-center text-white">
           <span className="text-[#912c2c] text-[50px] font-bold">A</span>sham
         </p>
-        <p>
+        <div className="flex flex-col items-center">
           <ins className="font-bold">Menu</ins>
-          <br />
+
           <Link className="" href={"AboutUs"}>
             About Us
           </Link>
-          <br />
+
           <Link className="" href={"ConatctUs"}>
             Contact Us
           </Link>
-          <br />
+
           <Link className="" href={"Terms"}>
             Terms & Conditions
           </Link>
-          <br />
-        </p>
-        <div>
-          <ins className="font-bold">Follow Us</ins>
-          <br />
-          <div className="border-2 border-[#912c2c] bg-[#0e0606] border-opacity-5 rounded-[5px] flex">
-            {imgs.map((curr) => {
-              return (
-                <div className="flex m-[5px]">
-                  <img className="w-[2rem]" src={curr} />
-                </div>
-              );
-            })}
+        </div>
+        <div className="">
+          <div className="flex flex-col items-center">
+            <ins className="font-bold">Follow Us</ins>
+            <div className="border-2 border-[#912c2c] bg-[#0e0606] border-opacity-5 rounded-[5px] flex ">
+              {imgs.map((curr) => {
+                return (
+                  <div className="flex m-[5px]">
+                    <img className="w-[2rem]" src={curr} />
+                  </div>
+                );
+              })}
+            </div>
           </div>
           <div className="flex items-center mt-[1rem]">
             <img className="w-[2rem]" src="/pho.png" />
@@ -49,7 +49,7 @@ export default function Footer() {
         </div>
       </div>
       <p></p>
-      <div className="m-auto w-[100%] flex items-center justify-center mt-[1rem]">
+      <div className="m-auto w-[100%] flex items-center justify-center mb-5 mt-[1rem]">
         <img className="w-[2rem]" src="/copyright.svg" />
         <p>{d.getFullYear()} asham.com All Rights Reserved</p>
       </div>

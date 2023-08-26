0524b3f7d1c3a72bd2a2d79631d9251e235195ac
abenikeb <abenikeb79@gmail.com>
2023-08-26 11:44:47 +0300

    modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ HOST="ftp.omishtujoy.com" # Replace with your cPanel FTP server hostname
 USERNAME="omishtujoyco@client-mvp.omishtujoy.com" # Replace with your cPanel FTP_username
 PASSWORD="1uj@21f6fwB" # Replace with your cPanel FTP password
 
-TARGETDIR="/client-mvp.omishtujoy.com/" # Replace with the target directory on your cPanel account
+TARGETDIR="/client-mvp.omishtujoy.com" # Replace with the target directory on your cPanel account
 
 echo "Deploying code to cPanel..."
 
